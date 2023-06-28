2d6534ac9cc9fb060d9d1685e8faca2464baa96a
snowlight-aemt <it.s.aemt@gmail.com>
2023-06-28 23:21:42 +0900

    Ch3 - 04 Job Parameter

--- src/main/java/me/snowlight/springbatch2_7/JobRunner.java
+++ src/main/java/me/snowlight/springbatch2_7/JobRunner.java
@@ -9,6 +9,12 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+
+// TODO 애플리케이션 실행 시 주입 방법
+//  java -jar spring-batch-2_7-0.0.1-SNAPSHOT.jar 'name=sdh' 'age(long)=22L' 'date(date)=2023/06/28' 'dollar(double)=33.4'
+//  자동 설정 켜고 아래에 컴포넌트 주석
+
 @Component
 public class JobRunner implements ApplicationRunner {
     private JobLauncher jobLauncher;
@@ -23,7 +29,10 @@ public class JobRunner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         JobParameters jobParameters = new JobParametersBuilder()
-                                            .addString("name", "sssss")
+                                            .addString("name", "Kim")
+                                            .addDouble("dollar", 101.5)
+                                            .addLong("age", 31L)
+                                            .addDate("date", new Date())
                                             .toJobParameters();
 
         this.jobLauncher.run(this.job, jobParameters);
