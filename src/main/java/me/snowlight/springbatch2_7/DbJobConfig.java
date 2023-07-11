51e899ee5643f37f712e78b9a07b2cf597162cae
snowlight-aemt <it.s.aemt@gmail.com>
2023-07-11 21:52:12 +0900

    Ch3 - 09 Execution Context

--- src/main/java/me/snowlight/springbatch2_7/DbJobConfig.java
+++ src/main/java/me/snowlight/springbatch2_7/DbJobConfig.java
@@ -3,16 +3,12 @@ package me.snowlight.springbatch2_7;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.Map;
 
 @Slf4j
 @Configuration
@@ -23,6 +19,11 @@ public class DbJobConfig {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
+    private final ExecutionContextTasklet1 executionContextTasklet1;
+    private final ExecutionContextTasklet2 executionContextTasklet2;
+    private final ExecutionContextTasklet3 executionContextTasklet3;
+    private final ExecutionContextTasklet4 executionContextTasklet4;
+
     @Bean
     public Job helloJob() {
         return jobBuilderFactory
@@ -30,35 +31,34 @@ public class DbJobConfig {
                 .start(step1())
                 .next(step2())
                 .next(step3())
+                .next(step4())
                 .build();
     }
     @Bean
     public Step step1() {
-        return stepBuilderFactory
-                .get("step1")
-                .tasklet(new CustomTasklet())
+        return stepBuilderFactory.get("step1")
+                .tasklet(executionContextTasklet1)
                 .build();
     }
 
     @Bean
     public Step step2() {
-        return stepBuilderFactory
-                .get("step2")
-                .tasklet((contribution, chunkContext) -> {
-                    log.info("# STEP2 has executed !!");
-                    return RepeatStatus.FINISHED;
-                })
+        return stepBuilderFactory.get("step2")
+                .tasklet(executionContextTasklet2)
                 .build();
     }
 
     @Bean
     public Step step3() {
-        return stepBuilderFactory
-                .get("step3")
-                .tasklet((contribution, chunkContext) -> {
-                    log.info("# STEP3 has executed !!");
-                    return RepeatStatus.FINISHED;
-                })
+        return stepBuilderFactory.get("step3")
+                .tasklet(executionContextTasklet3)
+                .build();
+    }
+
+    @Bean
+    public Step step4() {
+        return stepBuilderFactory.get("step4")
+                .tasklet(executionContextTasklet4)
                 .build();
     }
 }
--- src/main/java/me/snowlight/springbatch2_7/ExecutionContextTasklet4.java
+++ src/main/java/me/snowlight/springbatch2_7/ExecutionContextTasklet4.java
@@ -1,16 +1,20 @@
 package me.snowlight.springbatch2_7;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.stereotype.Component;
 
-@Slf4j
-public class CustomTasklet implements Tasklet {
+@Component
+public class ExecutionContextTasklet4 implements Tasklet {
+
     @Override
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-        log.info("# STEP1 Customer Tasklet !!");
+        System.out.println("Step4 execution !!");
+
+        Object name = chunkContext.getStepContext().getStepExecution().getJobExecution().getExecutionContext().get("name");
+        System.out.println("name : " + name);
 
         return RepeatStatus.FINISHED;
     }
