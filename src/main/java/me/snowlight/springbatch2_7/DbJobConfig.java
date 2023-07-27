16e484fa163dd327d09206abb8bdcc55d2bccf2b
snowlight-aemt <it.s.aemt@gmail.com>
2023-07-27 21:18:44 +0900

    Ch5 - 01 StepBuilderFactory

--- src/main/java/me/snowlight/springbatch2_7/DbJobConfig.java
+++ src/main/java/me/snowlight/springbatch2_7/DbJobConfig.java
@@ -7,11 +7,16 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.FlowBuilder;
 import org.springframework.batch.core.job.builder.SimpleJobBuilder;
+import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.*;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Slf4j
 @Configuration
@@ -23,10 +28,9 @@ public class DbJobConfig {
 
     @Bean
     public Job helloJob() {
-        SimpleJobBuilder simpleJobBuilder = jobBuilderFactory
-                .get("helloJOB1")
-                .start(step1())
+        SimpleJobBuilder simpleJobBuilder = jobBuilderFactory.get("helloJOB1")
                 .incrementer(new RunIdIncrementer())
+                .start(step1())
                 ;
 
         Job job = simpleJobBuilder.next(step2())
@@ -50,9 +54,24 @@ public class DbJobConfig {
     @Bean
     public Step step2() {
         return stepBuilderFactory.get("step2")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("Step2 Execution !!");
-                    return RepeatStatus.FINISHED;
+                .<String, String>chunk(3)
+                .reader(new ItemReader<String>() {
+                    @Override
+                    public String read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+                        return null;
+                    }
+                })
+                .processor(new ItemProcessor<String, String>() {
+                    @Override
+                    public String process(String item) throws Exception {
+                        return null;
+                    }
+                })
+                .writer(new ItemWriter<String>() {
+                    @Override
+                    public void write(List<? extends String> items) throws Exception {
+
+                    }
                 })
                 .build();
     }
@@ -60,10 +79,38 @@ public class DbJobConfig {
     @Bean
     public Step step3() {
         return stepBuilderFactory.get("step3")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("Step3 Execution !!");
-                    return RepeatStatus.FINISHED;
-                })
+                .partitioner(step1())
+                .gridSize(2)
                 .build();
+    }
+
+    @Bean
+    public Step step4() {
+        return stepBuilderFactory.get("step4")
+                .job(job())
+                .build();
+    }
+
+    @Bean
+    public Step step5() {
+        return stepBuilderFactory.get("step5")
+                .flow(flow())
+                .build();
+    }
+
+    @Bean
+    public Job job() {
+        return this.jobBuilderFactory.get("job")
+                .start(step1())
+                .next(step2())
+                .next(step3())
+                .build();
+    }
+
+    @Bean
+    public Flow flow() {
+        FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flow");
+        flowBuilder.start(step2()).end();
+        return flowBuilder.build();
     }
 }
